e63425c851b242136a6862fc83b05a62e74f36d1
SamySam2099 <38728942+SamySam2099@users.noreply.github.com>
2023-01-26 19:19:27 +0000

    Update schema

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const graphqlHTTP = require('express-graphql')
 const app = express()
 const PORT = 4000
 
+app.use('/graphql', graphqlHTTP({}))
+
 app.listen(PORT, () => {
   console.log(`now listening for requests on port ${PORT}`)
 })
