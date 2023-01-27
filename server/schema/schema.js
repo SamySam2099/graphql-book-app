86fa830684f7418a6a22328a4ac1eca84de1993a
Samuel Irsai <SIrsai@DCM-C02G9226ML85.(none)>
2023-01-27 18:45:16 +0000

    Applied queries in resolvers

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -37,6 +37,7 @@ const BookType = new GraphQLObjectType({
 			type: AuthorType,
 			resolve(parent, args) {
 				//return _.find(authors, { id: parent.authorID })
+				return Author.findById(parent.authorID)
 			},
 		},
 	}),
@@ -52,6 +53,7 @@ const AuthorType = new GraphQLObjectType({
 			type: new GraphQLList(BookType),
 			resolve(parent, args) {
 				//return _.filter(books, { authorID: parent.id })
+				return Book.find({ authorID: parent.id })
 			},
 		},
 	}),
@@ -70,6 +72,7 @@ const RootQuery = new GraphQLObjectType({
 			resolve(parent, args) {
 				// code to get data from db/other source
 				//return _.find(books, { id: args.id })
+				return Book.findById(args.id)
 			},
 		},
 		author: {
@@ -82,18 +85,22 @@ const RootQuery = new GraphQLObjectType({
 			resolve(parent, args) {
 				// code to get data from db/other source
 				//return _.find(authors, { id: args.id })
+				return Author.findById(args.id)
 			},
 		},
 		books: {
 			type: new GraphQLList(BookType),
 			resolve(parent, args) {
 				//return books
+				// that's how we return all books
+				return Book.find({})
 			},
 		},
 		authors: {
 			type: new GraphQLList(AuthorType),
 			resolve(parent, args) {
 				//return authors
+				return Author.find({})
 			},
 		},
 	},
